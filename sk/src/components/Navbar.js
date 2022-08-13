e3b7c826a1688de8f5db5d247ee31ad93e1c7d6c
Elif Kurtay <elif.kurtay00@gmail.com>
2022-08-13 16:40:30 +0300

    initial css

--- sk/src/components/Navbar.js
+++ sk/src/components/Navbar.js
@@ -12,34 +12,43 @@ function Navbar() {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  const showButton = () => {
+    if( window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+  window.addEventListener('resize', showButton)
+
   return (
     <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            SK Danışmanlık
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <FontAwesomeIcon icon={click ? faXmark : faBars}/>
-          </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Anasayfa
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                Hizmetlerimiz
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                İletişim
-              </Link>
-            </li>
-          </ul>
-          {button && <Button buttonStyle={"btn--outline"}>SIGN UP</Button>}
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          SK Danışmanlık
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <FontAwesomeIcon icon={click ? faXmark : faBars}/>
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Anasayfa
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Hizmetlerimiz
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              İletişim
+            </Link>
+          </li>
+        </ul>
+        {button && <Button buttonStyle={"btn--outline"}>SIGN UP</Button>}
+      </div>
     </nav>
   );
 }
