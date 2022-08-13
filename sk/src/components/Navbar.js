bee80c351ef7562eea12c0d7c374a5017909845f
Elif Kurtay <elif.kurtay00@gmail.com>
2022-08-13 16:57:04 +0300

    delete signup button

--- sk/src/components/Navbar.js
+++ sk/src/components/Navbar.js
@@ -2,24 +2,13 @@ import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
-import {Button} from "./Button";
 import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if( window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  }
-  window.addEventListener('resize', showButton)
 
   return (
     <nav className="navbar">
@@ -47,7 +36,6 @@ function Navbar() {
             </Link>
           </li>
         </ul>
-        {button && <Button buttonStyle={"btn--outline"}>SIGN UP</Button>}
       </div>
     </nav>
   );
