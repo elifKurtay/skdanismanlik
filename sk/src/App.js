3cf2696a85adb8a452ed84dadd31d892d817f1b7
elifkurtay <elif.kurtay00@gmail.com>
2024-01-31 13:22:16 +0100

    change browser router to hash

--- sk/src/App.js
+++ sk/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/pages/Home";
 
 function App() {
